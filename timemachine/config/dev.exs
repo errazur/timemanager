63939ed8d6e2ed6f504d385d006747e3c3fdea76
eminekarakurt67 <emine.karakurt@epitech.eu>
2023-11-13 13:22:14 +0100

    correct bdd password

--- timemachine/config/dev.exs
+++ timemachine/config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :timemachine, Timemachine.Repo,
   username: "postgres",
-  password: "root?",
+  password: "root",
   hostname: "localhost",
   database: "timemachine_dev",
   stacktrace: true,
